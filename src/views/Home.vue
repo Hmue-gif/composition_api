6272051ea37643d489b99053045f8d1d1909c90e
Hmue Thiha Naung <hmuethihanaung@emial.com>
2021-12-02 18:15:33 +0630

    ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <p> My name is {{name}} and I'm {{age}} year old </p>
+  <input type="text" v-model="person"> {{person}} 
+  <button @click="changeName">change</button>
+  <button @click="age++">increase</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+    let name = ref('Hmue');
+    let person = ref('')
+    let age = ref(20);
+    let changeName = ()=>{
+      console.log(name) //ref ka obj return pyn
+      return name.value ='Hmue Thiha Naung'
+    }
+    return{name,age,person,changeName}
   }
 }
 </script>
+
+<style>
+  button{
+    margin: 10px;
+  }
+</style>
